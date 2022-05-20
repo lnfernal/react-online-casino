77ca4ba56fb7b88e06092516ae98074dd290283a
nachiket-labelblind <nachiket.labelblind@gmail.com>
2022-05-20 23:02:25 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './Components/Header/Header'
+import Footer from './Components/Footer/Footer'
+import Content from './Components/Content/Content'
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
+
+import { ReactNotifications } from 'react-notifications-component'
+import 'react-notifications-component/dist/theme.css'
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <ReactNotifications />
+      <Header />
+      <Content />
+      <Footer />
     </div>
   );
 }
